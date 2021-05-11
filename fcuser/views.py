e31be9f48fa2cddac949ad1bd249baf057d10474
Mactaengoo <chs991209@gmail.com>
2021-05-11 18:54:22 +0900

    Tester_1 db 저장

--- fcuser/views.py
+++ fcuser/views.py
@@ -3,10 +3,6 @@ from .models import Fcuser
 
 # Create your views here.
 
-# register로 들어오려는 요청이 두 가지가 생기게 됨
-# 1. 웹사이트 url을 입력하여 접속하는 경우
-# 2.  제출 버튼을 클릭하여 접속하는 경우
-
 
 def register(request):
     if request.method == 'GET':
