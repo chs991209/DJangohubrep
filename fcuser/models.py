a3498577f1b485c724e9217ca8a4e48aeec2e816
Mactaengoo <chs991209@gmail.com>
2021-05-12 10:30:34 +0900

    Email Field 추가(Modelspy)

--- fcuser/models.py
+++ fcuser/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class Fcuser(models.Model):
     username = models.CharField(max_length=64,
                                 verbose_name='사용자명')
+    useremail = models.EmailField(max_length=128,
+                                 verbose_name='사용자 이메일')
     password = models.CharField(max_length=64,
                                 verbose_name='비밀번호')
     registered_dtm = models.DateTimeField(auto_now_add=True,
