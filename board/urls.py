8bbed72f884a8259e859c4bab5b7b64ec21a868f
Mactaengoo <chs991209@gmail.com>
2021-05-24 14:30:15 +0900

    views와 url 연결

--- board/urls.py
+++ board/urls.py
@@ -13,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from fcuser.views import home
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('fcuser/', include('fcuser.urls')),
-    path('board/', include('board.urls')),
-    path('', home),
+    path('list/', views.board_list)
 ]
