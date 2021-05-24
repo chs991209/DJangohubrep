ec7e8091beb5d78760fc308fda4f735947dd50a2
Mactaengoo <chs991209@gmail.com>
2021-05-24 11:13:36 +0900

    Login session form validation, board 생성

--- fcuser/forms.py
+++ fcuser/forms.py
@@ -4,8 +4,16 @@ from django.contrib.auth.hashers import check_password
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=32, label="사용자 이름")
-    password = forms.CharField(widget=forms.PasswordInput, label="비밀번호")
+    username = forms.CharField(
+        error_messages={
+            'required': '아이디를 입력해 주세요'
+        },
+        max_length=32, label="사용자 이름")
+    password = forms.CharField(
+        error_messages={
+            'required': '비밀번호를 입력해 주세요'
+        },
+        widget=forms.PasswordInput, label="비밀번호")
 
     def clean(self):
         cleaned_data = super().clean()
@@ -16,3 +24,6 @@ class LoginForm(forms.Form):
             fcuser = Fcuser.objects.get(username=username)
             if not check_password(password, fcuser.password):
                 self.add_error('password', '비밀번호를 틀렸습니다')
+
+            else:
+                self.user_id = fcuser.id
