e793f7012d74d1aa050c1c3fbc08b98c6e292684
Choi Hyunsu <chs991209@gmail.com>
2021-05-25 10:51:43 +0900

    Forms 수정

--- board/forms.py
+++ board/forms.py
@@ -1,29 +1,14 @@
 from django import forms
-from .models import Fcuser
-from django.contrib.auth.hashers import check_password
 
 
-class LoginForm(forms.Form):
-    username = forms.CharField(
+class BoardForm(forms.Form):
+    title = forms.CharField(
         error_messages={
-            'required': '아이디를 입력해 주세요'
+            'required': 'Enter the title'
         },
-        max_length=32, label="사용자 이름")
-    password = forms.CharField(
+        max_length=128, label="Title")
+    contents = forms.CharField(
         error_messages={
-            'required': '비밀번호를 입력해 주세요'
+            'required': 'Enter the contents'
         },
-        widget=forms.PasswordInput, label="비밀번호")
-
-    def clean(self):
-        cleaned_data = super().clean()
-        username = cleaned_data.get('username')
-        password = cleaned_data.get('password')
-
-        if username and password:
-            fcuser = Fcuser.objects.get(username=username)
-            if not check_password(password, fcuser.password):
-                self.add_error('password', '비밀번호를 틀렸습니다')
-
-            else:
-                self.user_id = fcuser.id
+        widget=forms.Textarea, label="Contents")
